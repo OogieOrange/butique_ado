8911d5622eb9ad2dad8a29b756d511d9e64e87c1
OogieOrange <>
2023-05-29 12:41:56 +0000

    Add toasts

--- butique_ado/settings.py
+++ butique_ado/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STOREGE = 'django.contrib.message.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
